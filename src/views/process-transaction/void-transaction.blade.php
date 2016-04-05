1d325e4c0b31d57630f97270041ae4e72b61c28b
Craig Wann <craigwann1@gmail.com>
2016-04-05 13:19:43 -0500

    void entityId and locationId were capped

--- src/views/process-transaction/void-transaction.blade.php
+++ src/views/process-transaction/void-transaction.blade.php
@@ -4,8 +4,8 @@
 		<pv:VoidTransaction>
 			<pv:storeId>{{ config('profit-stars.store-id', env('PROFIT_STARS_STORE_ID')) }}</pv:storeId>
 			<pv:storeKey>{{ config('profit-stars.store-key', env('PROFIT_STARS_STORE_KEY')) }}</pv:storeKey>
-			<pv:EntityId>{{ config('profit-stars.entity-id', env('PROFIT_STARS_ENTITY_ID')) }}</pv:EntityId>
-			<pv:LocationId>{{ config('profit-stars.location-id', env('PROFIT_STARS_LOCATION_ID')) }}</pv:LocationId>
+			<pv:entityId>{{ config('profit-stars.entity-id', env('PROFIT_STARS_ENTITY_ID')) }}</pv:entityId>
+			<pv:locationId>{{ config('profit-stars.location-id', env('PROFIT_STARS_LOCATION_ID')) }}</pv:locationId>
 			<pv:originalReferenceNumber>{{ $originalReferenceNumber }}</pv:originalReferenceNumber>
 		</pv:VoidTransaction>
 	</soapenv:Body>
